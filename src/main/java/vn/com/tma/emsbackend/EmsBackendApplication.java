afe54a22cb704011f69736a0d665848b424ef94d
ductuan15 <47861874+ductuan15@users.noreply.github.com>
2022-03-05 23:28:13 +0700

    EMS-29 Init logging to application

--- src/main/java/vn/com/tma/emsbackend/EmsBackendApplication.java
+++ src/main/java/vn/com/tma/emsbackend/EmsBackendApplication.java
@@ -5,9 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class EmsBackendApplication {
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(false);
+		return loggingFilter;
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsBackendApplication.class, args);
