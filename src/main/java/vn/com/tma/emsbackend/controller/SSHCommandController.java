3f4b5b8c8eafff2fdf74c520fde8b3559414ba31
ductuan15 <tuanduc1506@gmail.com>
2022-05-31 20:50:45 +0700

    EMS-63 Delete useless code

--- src/main/java/vn/com/tma/emsbackend/controller/SSHCommandController.java
+++ src/main/java/vn/com/tma/emsbackend/controller/SSHCommandController.java
@@ -7,14 +7,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import vn.com.tma.emsbackend.model.dto.ErrorDTO;
 import vn.com.tma.emsbackend.model.dto.SSHCommandDTO;
 import vn.com.tma.emsbackend.model.dto.SSHCommandResponseDTO;
 import vn.com.tma.emsbackend.service.device.NetworkDeviceService;
 
-import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @RestController
@@ -33,22 +31,6 @@ public class SSHCommandController {
     @PostMapping()
     public SSHCommandResponseDTO sendCommandToDeviceById(@PathVariable(value = "id") Long deviceId, @RequestBody SSHCommandDTO sshCommandDTO) {
         return networkDeviceService.sendCommandToDeviceById(deviceId, sshCommandDTO);
-    }
-
-
-    @Operation(summary = "Download configuration file from network device")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully get configuration file from device", content = {
-                    @Content(schema = @Schema(implementation = byte[].class))}),
-            @ApiResponse(responseCode = "502", description = "Have a error while get configuration from device", content = {
-                    @Content(schema = @Schema(implementation = ErrorDTO.class))}),
-    })
-    @ResponseStatus(code = HttpStatus.ACCEPTED)
-    @GetMapping(path = "/configuration", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_OCTET_STREAM_VALUE})
-    public byte[] downloadDeviceConfigFileById(@PathVariable(value = "id") Long deviceId, HttpServletResponse response) {
-        byte[] bytes = networkDeviceService.downloadDeviceConfigFileById(deviceId);
-        response.setHeader("Content-Disposition", "attachment; filename=\"" + "configuration_" + deviceId + "\"");
-        return bytes;
     }
 
 }
